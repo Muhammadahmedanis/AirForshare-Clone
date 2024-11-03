bfb52ae34c1251d6722e58b57684912c19847d8e
Muhammad Ahmed <153613965+Muhammadahmedanis@users.noreply.github.com>
2024-11-04 01:17:53 +0500

    work on functionality

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -7,11 +7,12 @@ import { FaPlus } from "react-icons/fa6";
 import { SiCss3 } from "react-icons/si";
 import { IoLogoJavascript } from "react-icons/io";
 
-function FileList({ file, onDrop }) {
+function FileList({ tempFile ,file, onDrop }) {
     console.log(file);
   return (
     <div className='files-list'>
-            { file.map((val, ind) =>{
+
+            { file?.map((val, ind) =>{
                 let icon;
                 switch (val.type) {
                     case 'text/html':
@@ -29,7 +30,40 @@ function FileList({ file, onDrop }) {
                 }
 
             return (
-                    <div key={ind}> 
+                    <a href={val.url} target='_blank' download key={ind}>
+                        <div> 
+                        {
+                            val.type.indexOf('image') !== -1 ? <img height={70} width={70} src={val.url} alt="" /> :
+                            <>
+                                {icon} 
+                                <span>{val.name.slice(0, 15)}</span>
+                            </>
+                        }
+                        </div>
+                    </a>
+                    )
+                }
+            )}
+
+            { tempFile.map((val, ind) =>{
+                let icon;
+                switch (val.type) {
+                    case 'text/html':
+                        icon = <ImHtmlFive2 size={38} />
+                        break;
+                    case 'text/css':
+                        icon = <SiCss3 size={38} />
+                        break;
+                    case 'text/javascript':
+                        icon = <IoLogoJavascript size={38} />
+                        break;
+                    default:
+                        icon =<CiFileOn size={38} />
+                        break;
+                }
+
+            return (
+                    <div className='temp-file' key={ind}> 
                     {
                         val.type.indexOf('image') !== -1 ? <img height={70} width={70} src={URL.createObjectURL(val)} alt="" /> :
                         <>
@@ -37,12 +71,15 @@ function FileList({ file, onDrop }) {
                             <span>{val.name.slice(0, 15)}</span>
                         </>
                     }
+                    <img className='loader' src="https://global.discourse-cdn.com/sitepoint/original/3X/e/3/e352b26bbfa8b233050087d6cb32667da3ff809c.gif" alt="" />
                     </div>
                     )
                 }
-            )
-        }
+            )}
+
         <div>
+            
+
         {/* <b>{val.name.slice(val.name.lastIndexOf('.'))}</b>  */}
             <DropZone onDrop={onDrop} title={
                 <>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,22 +10,39 @@ import { FaDownload } from "react-icons/fa6";
 import { MdDelete } from "react-icons/md";
 import { db, ref, set, onValue, remove, storage, uploadBytesResumable, storageRef, getDownloadURL} from '../../db';
 import CodeEditor from '../../components/CodeEditor';
+// import JSZip from  'jszip';
 
 function Home() {
     const[type, setType] = useState('text');
     const[text, setText] = useState('');
     const[file, setFile] = useState([]);
     const[isText, setIsText] = useState(false);
+    const[tempFile, setTempFile] = useState([]);
 
-    const onDrop = acceptedFiles => {
-        setFile([...file, ...acceptedFiles])
-        uploadFile(acceptedFiles[0], 0);
+    const onDrop = async (acceptedFiles) => {
+        setTempFile((prevTempFile) => [...prevTempFile, ...acceptedFiles])
+        let arr = [];
+        for(let i = 0; i < acceptedFiles.length; i++){
+            arr.push(uploadFile(acceptedFiles[i], file?.length + i));
+        }
+        const allFiles = await Promise.all(arr);
+        setFile((prevFiles = []) => {
+            const updFiles = [...prevFiles, ...allFiles]
+            console.log(prevFiles);
+            set(ref(db, 'file-sharing'), {
+                files: updFiles,
+            });
+            return updFiles;
+        });
+        setTempFile([]);
     }
+    console.log("Files: ", file);
 
-    const uploadFile = (file, i) => {
-        const storageFile = storageRef(storage, `files/file-${i}`);
-        const uploadTask = uploadBytesResumable(storageFile, file);
-        uploadTask.on('state_changed', 
+    const uploadFile = (files, i) => {
+        return new Promise((resolve, reject) => {
+            const storageFile = storageRef(storage, `files/file-${i}`);
+            const uploadTask = uploadBytesResumable(storageFile, files);
+            uploadTask.on('state_changed', 
         (snapshot) => {
             const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
             console.log('Upload is ' + progress + '% done');
@@ -33,43 +50,81 @@ function Home() {
             case 'paused':
                 console.log('Upload is paused');
                 break;
-            case 'running':
-                console.log('Upload is running');
-                break;
-            }
+                case 'running':
+                    console.log('Upload is running');
+                    break;
+                }
         }, 
         (error) => {
-            console.log(error);
+            reject(error);
         }, 
         () => {
             getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            console.log('File available at', downloadURL);
+            resolve({url: downloadURL, type: files.type, name: files.name});
             });
         }
         );
+    })
     }
 
     const saveChanges = () => {
-        set(ref(db, 'sharing'), {
+        set(ref(db, 'text-sharing'), {
             textValue: text,
         });
     }
 
     const clearText = async() => {
-        await remove(ref(db, 'sharing'));
+        await remove(ref(db, 'text-sharing'));
         setText('');
         setIsText(false);
     }
 
+    const deleteAllFiles = async() => {
+        await remove(ref(db, 'file-sharing'));
+        setFile([]);
+    }
+
+    // const downloadAll = () => {
+    //     let filename = "MultiFilesDownload";
+    //     const urls = file?.map(val => val.url)
+    //       const zip = new JSZip()
+    //       const folder = zip.folder('project')
+    //       urls.forEach((url)=> {
+    //      const blobPromise =  fetch(url)    
+    //   .then(function (response) {  
+    //     console.log({response})             
+    //       if (response.status === 200 || response.status === 0) {
+    //           return Promise.resolve(response.blob());
+    //       } else {
+    //           return Promise.reject(new Error(response.statusText));
+    //       }
+    //   })                          
+    //    const name = url.substring(url.lastIndexOf('/'))
+    //           folder.file(name, blobPromise)
+    //       })
+      
+    //       zip.generateAsync({type:"blob"})
+    //           .then(blob => saveAs(blob, filename))
+    //           .catch(e => console.log(e));
+    // }
+
     useEffect(() => {
-        const starCountRef = ref(db, 'sharing');
-        onValue(starCountRef, (snapshot) => {
+        const textRef = ref(db, 'text-sharing');
+        onValue(textRef, (snapshot) => {
         const data = snapshot.val();
         setText(data?.textValue)
         if(data?.textValue){
             setIsText(true)
         }
         });
+
+        const fileRef = ref(db, 'file-sharing');
+        onValue(fileRef, (snapshot) => {
+        const data = snapshot.val();
+        setFile(data?.files || [])
+        });
+
+
     }, [])
 
     const links = text?.match(/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g)
@@ -152,13 +207,13 @@ function Home() {
                                     <div className='download-btn'>
                                         <div><FaDownload size={22} /> Download All</div>
                                     </div>
-                                    <div onClick={() => setFile([])} className='delete-btn'>
+                                    <div onClick={deleteAllFiles} className='delete-btn'>
                                         <div><MdDelete size={22} /> Delete All</div>
                                     </div>
                                 </div>
                             </div>
                             {
-                                file.length ? <FileList file={file} onDrop={onDrop} /> :
+                                tempFile?.length || file?.length ? <FileList tempFile={tempFile} file={file} onDrop={onDrop} /> :
                                 <DropZone 
                                 onDrop={onDrop}
                                 title={
